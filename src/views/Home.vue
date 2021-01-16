c67df3173f0d3011833a3f15162f6ce44cb1070c
Ionhedblom <ionysoshedblom@gmail.com>
2021-01-16 16:52:44 +0100

    Project complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     <aside class="confirmation-modal" v-if="confirmModal">
       <h2>Are you sure? This can't be undone.</h2>
     </aside>
-    <a class="btn delete" href="#">remove card</a>
+
     <CardStack
       v-bind:isActive="active"
       v-bind:setActive="setNewActiveCard"
@@ -32,7 +32,6 @@ export default {
     return {
       cards: this.$root.cards,
       active: 0,
-      confirmModal: false,
     };
   },
   methods: {
@@ -48,24 +47,4 @@ export default {
 </script>
 
 
-<style scoped>
-.delete {
-  background: #ff574b;
-  width: 40%;
-  font-size: 0.9rem;
-  height: 2rem;
-  margin: 8px auto 0 auto;
-  box-shadow: inset 0 0 20px rgba(255, 255, 255, 0);
-  color: #fff;
-  border: 0.1rem solid rgb(104, 104, 104);
-  border-radius: 0.3rem;
-}
 
-.delete:hover {
-  box-shadow: inset 0 0 20px rgba(199, 199, 199, 0.5),
-    0 0 20px rgba(189, 189, 189, 0.2);
-
-  border: none;
-  text-shadow: 0.2px 0.2px 0.5px #427388;
-}
-</style>
