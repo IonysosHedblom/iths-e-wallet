779069b38b0952c70e52dbe566c76fa3a6b7586c
Ionhedblom <ionysoshedblom@gmail.com>
2021-01-18 21:22:38 +0100

    Remove button added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main>
     <Top header="e-wallet" subheader="active card" />
     <Card v-if="cards[active]" v-bind:cardData="cards[active]" />
+    <a href="#" class="btn delete">Remove card</a>
     <CardStack
       v-bind:isActive="active"
       v-bind:setActive="setNewActiveCard"
@@ -42,5 +43,14 @@ export default {
 };
 </script>
 
-
-
+<style scoped>
+.delete {
+  width: 40%;
+  font-size: 0.9rem;
+  height: 2rem;
+  color: #fff;
+  border: 0.1rem solid rgb(41, 27, 27);
+  background-color: rgb(255, 59, 59);
+  margin: 7px auto 0 auto;
+}
+</style>
